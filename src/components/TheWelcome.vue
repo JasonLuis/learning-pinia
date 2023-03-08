335a00ec00e466ec3df8e99cb64efaef968123d1
JasonLuis <jason20luis@gmail.com>
2023-03-08 10:33:06 -0300

    chore: create project to learning pinia

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { useCounterStore } from '../stores/counter';
+</script>
+
+<template>
+  <div>
+    <p>teste {{ useCounterStore().count }}</p>
+    <button style="width: 200px; height: 30px; border-radius: 5px;" @click="useCounterStore().increment">Incrementar</button>
+  </div>
+</template>
